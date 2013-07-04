0fcaa88a4c030db3e42192e87bb82f513ffd7d4e
njitdkj082yy <njitdkj082yy@sina.com>
2013-07-04 15:26:21 +0800

    [math]:let,expr and bc study

--- math.sh
+++ math.sh
@@ -9,3 +9,43 @@ let no1+=1
 let no2--
 echo $no1
 echo $no2
+echo "***************[ ]****************"
+echo 'result=$[ no1 + no2 ]'
+result=$[ no1 + no2 ]
+echo $result
+echo 'result=$[ $no1 + no2 ]'
+result=$[ $no1 + no2 ]
+echo $result
+echo 'result=$((no1 + no2))'
+result=$((no1 + no2))
+echo $result
+echo 'result=$(($no1 + no2))'
+result=$(($no1 + no2))
+echo $result
+echo "***************[ ]****************"
+
+echo "***************expr**************"
+echo "result=`expr 3 + 4`"
+result=`expr 3 + 4`
+echo $result
+
+echo 'result=$(expr $no1 + 5)'
+result=$(expr $no1 + 5)
+echo $result
+echo "***************expr**************"
+
+echo "***************bc***************"
+echo "4 * 0.5" | bc
+echo "scale=3;3/8" | bc
+echo "***************bc***************"
+
+echo "***************bc 2<->10***************"
+no=100
+echo "obase=2;ibase=10;$no" | bc
+
+no=1100100
+echo "obase=10;ibase=2;$no" | bc
+echo "***************bc 2<->10***************"
+
+echo "sqrt(100)" | bc
+echo "10^10" | bc
