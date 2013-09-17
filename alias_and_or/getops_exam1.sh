307adc09fc678e3210755ade726699e3807ab336
yy <yy@ubuntu10.04-desktop>
2013-09-17 18:34:45 +0800

    change compare string

--- alias_and_or/getops_exam1.sh
+++ alias_and_or/getops_exam1.sh
@@ -4,7 +4,7 @@ func()
 	echo " 'basename $0' -[a b] args." >&2
 	exit 0
 }
-while getopts "ba:" options
+while getopts "[ba:]" options
 do
     case $options in
     a)
@@ -14,9 +14,11 @@ do
 	echo "You enter -b as an option."
   	;;
     \?)
+	echo "You enter ? as an option."
 	func
 	;;
     :)
+	echo "You enter : as an option"
 	echo "No argument value for option $OPTARG"
 	;;
     esac
