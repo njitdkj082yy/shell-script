94c1d03ea684d821ad8576786d2d6e6a07650b2e
yy <yy@ubuntu10.04-desktop>
2013-09-18 17:31:12 +0800

    color study

---
+++ alias_and_or/print-ascii.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+start=48
+end=57
+echo "Decimal	Hex	Character"
+echo "------	---	---------"
+for (( i=start;i<=end;i++ ))
+do
+	echo $i | awk '{printf(" %3d	%2x	%c\n",$1,$1,$1)}'
+done
+exit 0
